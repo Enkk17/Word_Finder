fdffcc3129e978848b246d46436882ae83cfe660
Enkk17 <86929986+Enkk17@users.noreply.github.com>
2022-02-20 19:30:13 +0100

    Update finder.cpp

--- finder.cpp
+++ finder.cpp
@@ -1,45 +1,85 @@
-#include <iostream>
-#include <string>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
+
+//dichiariamo le funzioni
+string file_content();
+bool badchar(char c);
+int has_word(string pattern, string text);
+
 
 int main()
 {
-	
-	string research;
-	bool verify=false;
-	cout<<"enter what you want to search for: ";
-	
-	getline(cin,research);
-	
-	// filestream variables
-    fstream file;
-    string word, t, q, filename;
-  
-    // we declare the variable filename which corresponds to the name of the file
-    filename = "file.txt";
-  
-    //open file
-    file.open(filename.c_str());
-    
-    
-    //we declare for loop to read all the words in the file
-    for(int x=0 ; file >> word; x++)
-    {
-    	//comparison of words with user input
-    	if(word==research)
-    	{
-		verify=true;
-    	}
-    }
-    
-    //print the result
-    if(verify==true)
-    {
-    cout<<"the word " << research << " is present in the text!";
-	}else
+string pattern;
+cout<<"inserisci la parola che stai cercando : ";
+cin>>pattern;
+
+//lettura del file
+string text = file_content();
+
+
+//sostituiamo tutti i badchar con uno spazio
+replace_if(text.begin(), text.end(), badchar, ' ');
+
+int conteggio = has_word(pattern, text);
+
+
+//stampa dei risultati
+
+if (conteggio == 0) 
 	{
-	cout<<"the word " << research << " is not present in the text!";
+  	cout << "la parola " << pattern << " non  e presente nel testo!";
+	} else {
+  	cout << "la parola " << pattern << " e' presente nel testo ed e' presente " << conteggio << " volte! ";
 	}
-    return 0;
+
+return 0;
+}
+
+//questa funzione prende il contenuto del file
+string file_content() 
+{
+	// filestream variabili
+    fstream file;
+    string filename;
+ 
+    //dichiariamo filename che corrisponde al file che vogliamo aprire
+    filename = "file.txt";
+ 
+    //apertura del file
+    file.open(filename.c_str());
+  	
+	// crea uno stringstream
+	stringstream buffer;  
+	
+	// passa allo stringstream il buffer del file
+	buffer << file.rdbuf();
+
+	//lettura file
+	return buffer.str(); 
+  	
+}
+
+//questa funzione controlla se pattern è una parola (stringa separata da spazi) in text.
+int has_word(string pattern, string text)
+ {
+  istringstream stream(text);
+  int contatore=0;
+  for (string word; stream >> word;)  // per ogni parola nel testo
+	{
+		if(word == pattern) 
+		{
+		contatore++;
+		}	
+	}
+	return contatore;
+}
+
+// Tutto ciò che non è alfabetico né uno spazio è "bad" (da scartare).
+bool badchar(char c) 
+{ 
+return !isalpha(c) && !isspace(c); 
 }
